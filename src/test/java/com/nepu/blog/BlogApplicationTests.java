d025aab9a5b13e829f20c73e55d764812c3819ca
bryan <zr_bryan@163.com>
2019-10-21 09:17:38 +0800

    one

---
+++ src/test/java/com/nepu/blog/BlogApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nepu.blog;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
